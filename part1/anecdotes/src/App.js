699a7d38f4aa043d1b600279e94f1607efd33be3
ori <93719767+oriodev@users.noreply.github.com>
2023-04-23 19:26:43 +0100

    anecdotes

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+const App = () => {
+
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length))
+  const [biggest, setBiggest] = useState(0)
+
+  const generateAnecdote = () => {
+    const num = Math.floor(Math.random() * (anecdotes.length - 1))
+    setSelected(num)
+  }
+
+  const findBiggest = () => {
+    let largest = 0;
+
+    for (var i = 0; i < points.length; i++) {
+        if (largest < points[i] ) {
+            largest = i;
+        }
+    }
+
+    setBiggest(largest)
+  }
+
+  const updateVotes = () => {
+    const copy = [ ...points ]
+    copy[selected] += 1
+    setPoints(copy)
+    findBiggest()
+  }
+
+  return (
+    <div>
+      <h1>anecdote of the day</h1>
+      {anecdotes[selected]}
+      <p>has {points[selected]} votes</p>
+      <br />
+      <button onClick={updateVotes}>vote</button>
+      <button onClick={generateAnecdote}>next anecdote</button>
+
+      <h1>anecdote with the most votes</h1>
+      {anecdotes[biggest]}
+      
+    </div>
+  )
+}
+
+export default App
