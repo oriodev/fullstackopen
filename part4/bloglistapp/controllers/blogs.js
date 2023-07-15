7f0d38146bca1de958ded79aae57e60448407066
ori <93719767+oriodev@users.noreply.github.com>
2023-07-15 17:23:25 +0100

    changed one tiny thing and it didn't break whoo

--- part4/bloglistapp/controllers/blogs.js
+++ part4/bloglistapp/controllers/blogs.js
@@ -9,7 +9,16 @@ blogsRouter.get('/', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
   try {
-    const blog = new Blog(request.body)
+    const body = request.body
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    })
+
+
     const result = await blog.save()
     response.status(201).json(result)
   } catch {
