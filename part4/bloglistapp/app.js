3e8c2719a422b7dc51fdf0e4d786e69b2390424d
ori <93719767+oriodev@users.noreply.github.com>
2023-07-15 18:35:28 +0100

    error handling done whoop whoop

--- part4/bloglistapp/app.js
+++ part4/bloglistapp/app.js
@@ -1,18 +1,28 @@
 const config = require('./utils/config')
 const express = require('express')
-require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+require('express-async-errors')
+
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-const mongoose = require('mongoose')
+
+const middleware = require('./utils/middleware')
+
   
 mongoose.connect(config.MONGODB_URI)
   
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglistapp/controllers/users.js
+++ part4/bloglistapp/controllers/users.js
@@ -7,25 +7,29 @@ usersRouter.get('/', async (request, response) => {
   response.json(users)
 })
 
-usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+usersRouter.post('/', async (request, response, next) => {
+  try {
+    const { username, name, password } = request.body
 
-  if (password.length <= 3) {
-    return response.status(400).json({ error: 'password is too short'})
+    if (password.length <= 3) {
+      return response.status(400).json({ error: 'password is too short'})
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+  } catch(error) {
+    next(error)
   }
-
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
-
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
-
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
