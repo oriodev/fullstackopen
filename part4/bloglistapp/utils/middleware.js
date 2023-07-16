bf2159511050a8dbf6defdf52a9af57e5bb47311
ori <93719767+oriodev@users.noreply.github.com>
2023-07-16 10:43:55 +0100

    p sure i made the token extractor middleware work

--- part4/bloglistapp/utils/middleware.js
+++ part4/bloglistapp/utils/middleware.js
@@ -30,7 +30,12 @@ const errorHandler = (error, request, response, next) => {
 
 const tokenExtractor = (request, response, next) => {
   // code that extracts the token
-  console.log('token thing')
+ 
+  const authorization = request.get('authorization')  
+  
+  if (authorization && authorization.startsWith('Bearer ')) {    
+    request.token = authorization.replace('Bearer ', '')
+  }  
 
   next()
 }
